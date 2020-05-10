0092e3fc0a5d0e15aaac8196adb84470258ffaeb
Drew Theran <drew.theran@gmail.com>
2020-05-10 09:24:12 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import DatePicker from 'react-native-datepicker';
 import { Text, View, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -61,14 +63,38 @@ class Reservation extends Component {
                 },
                 {
                     text: "OK",
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm()
+                    }
                 }
             ]           
             )
         }
 
+        async obtainNotificationPermission() {
+            const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+                if (permission.status !== 'granted') {
+                    Alert.alert('Permission not granted to show notifications');
+                }
+                return permission;
+            }
+            return permission;
+        }
+
+        async presentLocalNotification(date) {
+            const permission = await this.obtainNotificationPermission();
+            if (permission.status === 'granted') {
+                Notifications.presentLocalNotificationAsync({
+                    title: 'Your Campsite Reservation Search',
+                    body: 'Search for ' + date + ' requested'
+                });
+            }
+        }
+
     render() {
-        
         return (
             <Animatable.View 
             animation='zoomIn'
